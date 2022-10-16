d940f2035a0fbb734e650e0dd4598cbc4f34a716
DamarisCandia <damaris.candia1701@alumnos.ubiobio.cl>
2022-10-16 18:08:52 -0300

    caso 3 avance en primera parte

--- IA.java
+++ IA.java
@@ -80,55 +80,325 @@ public class IA {
             }
         }
 		switch(turno) {
-		
-	    	case 1 :                                             //ia = 2
-	    		
-	    		if(t[1][1] == 1) {
-	    			int e = (int)(Math.random() * 4)+1;
-	    			switch(e) {
-	    			
-		    			case 1: d = 1;
-		    					break;
-		    					
-		    			case 2: d = 3;
-		    					break;
-		    					
-		    			case 3: d = 7;
-		    					break;
-		    					
-		    			case 4: d = 9;
-		    					break;
-		    					
-		    		}
-	    		}else {
-	    			d = 5;
-	    		}
-	    		break;
-	    		
-	    	case 2 :                                            //ia = 1
-	    		break;
-	    		
-	    	case 3 :                                            //ia  = 2
-	    		break;
-	    		
-	    	case 4 :                                            //ia = 1
-	    		break;
-	    		
-	    	case 5 :      										//ia = 2                           
-	    		break;
-	    		
-	    	case 6 :											//ia = 1
-	    		break;
-	    		
-	    	case 7 :											//ia = 2
-	    		break;
-	    		
-	    	case 8 :											//ia = 1
-	    		break;
-	    		
-	    	case 9 :											//ia = 2
-	    		break;
-	    		
+
+			case 1:                                             //ia = 2
+
+				if (t[1][1] == 1) {
+					int e = (int) (Math.random() * 4) + 1;
+					switch (e) {
+
+						case 1:
+							d = 1;
+							break;
+
+						case 2:
+							d = 3;
+							break;
+
+						case 3:
+							d = 7;
+							break;
+
+						case 4:
+							d = 9;
+							break;
+
+					}
+				} else {
+					d = 5;
+				}
+				break;
+
+			case 2:                                            //ia = 1
+
+				int e;
+				if (t[0][0] == 2 || t[0][2] == 2 || t[2][0] == 2 || t[2][2] == 2) {    // si el rival esta en una esquina se busca empate (asumiendo que no cometa errores)
+					e = 0;
+					if (t[0][0] == 2) {        // defino que esquina es en la que esta la jugada del rival
+						e = 1;
+					}
+					if (t[0][2] == 2) {
+						e = 2;
+					}
+					if (t[2][0] == 2) {
+						e = 3;
+					}
+					if (t[2][2] == 2) {
+						e = 4;
+					}
+
+					switch (e) {                              //Segun la esquina escojo uno de sus lados para que su siguiente jugada no pueda ser una linea
+						case 1:									//1  (posicion)
+							e = (int) (Math.random() * 2) + 1;
+							if (e == 1) {
+								d = 2;
+							}else {
+								d = 4;
+							}
+							break;
+
+						case 2:									//3
+							e = (int) (Math.random() * 2) + 1;
+							if (e == 1) {
+								d = 2;
+							}else {
+								d = 6;
+							}
+							break;
+
+						case 3:									//7
+							e = (int) (Math.random() * 2) + 1;
+							if (e == 1) {
+								d = 4;
+							}else {
+								d = 8;
+							}
+							break;
+
+						case 4:									//9
+							e = (int) (Math.random() * 2) + 1;
+							if (e == 1) {
+								d = 6;
+							}else {
+								d = 8;
+							}
+							break;
+					}
+
+
+				} else {                                                               // si el rival esta en un lado se busca victoria con doble esquina
+					e = 0;
+					if (t[0][1] == 2) {        // defino que posicion es en la que esta la jugada del rival
+						e = 1;
+					}
+					if (t[1][0] == 2) {
+						e = 2;
+					}
+					if (t[1][2] == 2) {
+						e = 3;
+					}
+					if (t[2][1] == 2) {
+						e = 4;
+					}
+					switch (e) {                              //Segun el lado escojo una de sus esquinas para buscar la victoria en el siguiente turno
+						case 1:									// 2
+							e = (int) (Math.random() * 2) + 1;
+							if (e == 1) {
+								d = 1;
+							}else {
+								d = 3;
+							}
+							break;
+
+						case 2:                               // 4
+							e = (int) (Math.random() * 2) + 1;
+							if (e == 1) {
+								d = 1;
+							}else {
+								d = 7;
+							}
+							break;
+
+						case 3:                               // 6
+							e = (int) (Math.random() * 2) + 1;
+							if (e == 1) {
+								d = 3;
+							}else {
+								d = 9;
+							}
+							break;
+
+						case 4:                               // 8
+							e = (int) (Math.random() * 2) + 1;
+							if (e == 1) {
+								d = 7;
+							}else {
+								d = 9;
+							}
+							break;
+					}
+				}
+				break;
+
+			case 3:                                            //ia  = 2
+
+				if(t[1][1] == 1){                              // rival tiene el centro, se debe bloquear si o si
+					e = 0;
+					int cont = 0;
+					for(int i = 0; i < t.length; i++){
+						for(int j = 0; j < t.length; j++){
+							cont++;
+							if(t[i][j] == 1 && cont != 5){
+								e = cont;
+							}
+						}
+					}
+					switch (e){          // se revisara si no se dio una jugada al lado opuesto en que la ia jugo, si no se jugara defendiendo, de ser el caso se jugara buscando armar una linea
+						case 1:
+							d = 9;
+							if(t[2][2] == 2){                                //compruebo si jugo en el opuesto a mi primer jugada
+								e = (int) (Math.random() * 4) + 1;           //escojo una de las 4 opciones en donde podria formar una linea
+								switch (e){
+									case 1:
+										d = 3;
+										break;
+									case 2:
+										d = 6;
+										break;
+									case 3:
+										d = 7;
+										break;
+									case 4:
+										d = 8;
+										break;
+								}
+							}
+							break;
+
+						case 2:
+							d = 8;
+							if(t[2][1] == 2){
+								e = (int) (Math.random() * 2) + 1;
+								switch (e){
+									case 1:
+										d = 7;
+										break;
+									case 2:
+										d = 9;
+										break;
+								}
+							}
+							break;
+						case 3:
+							d = 7;
+							if(t[2][0] == 2){
+								e = (int) (Math.random() * 4) + 1;           //escojo una de las 4 opciones en donde podria formar una linea
+								switch (e){
+									case 1:
+										d = 1;
+										break;
+									case 2:
+										d = 4;
+										break;
+									case 3:
+										d = 8;
+										break;
+									case 4:
+										d = 9;
+										break;
+								}
+							}
+							break;
+						case 4:
+							d = 6;
+							if(t[1][2] == 2){
+								e = (int) (Math.random() * 2) + 1;
+								switch (e){
+									case 1:
+										d = 3;
+										break;
+									case 2:
+										d = 9;
+										break;
+								}
+							}
+							break;
+						case 6:
+							d = 4;
+							if(t[1][0] == 2){
+								e = (int) (Math.random() * 2) + 1;
+								switch (e){
+									case 1:
+										d = 1;
+										break;
+									case 2:
+										d = 7;
+										break;
+								}
+							}
+							break;
+						case 7:
+							d = 3;
+							if(t[0][2] == 2){
+								e = (int) (Math.random() * 4) + 1;           //escojo una de las 4 opciones en donde podria formar una linea
+								switch (e){
+									case 1:
+										d = 1;
+										break;
+									case 2:
+										d = 2;
+										break;
+									case 3:
+										d = 6;
+										break;
+									case 4:
+										d = 9;
+										break;
+								}
+							}
+							break;
+
+						case 8:
+							d = 2;
+							if(t[0][1] == 2){
+								e = (int) (Math.random() * 2) + 1;
+								switch (e){
+									case 1:
+										d = 1;
+										break;
+									case 2:
+										d = 3;
+										break;
+								}
+							}
+							break;
+						case 9:
+							d = 1;
+							if(t[0][0] == 2){
+								e = (int) (Math.random() * 4) + 1;           //escojo una de las 4 opciones en donde podria formar una linea
+								switch (e){
+									case 1:
+										d = 2;
+										break;
+									case 2:
+										d = 3;
+										break;
+									case 3:
+										d = 4;
+										break;
+									case 4:
+										d = 7;
+										break;
+								}
+							}
+							break;
+					}
+
+				}else{                                         // ia tiene el centro
+
+						//terminar
+				}
+
+				break;
+
+			case 4:                                            //ia = 1
+				break;
+
+			case 5:                                            //ia = 2
+				break;
+
+			case 6:                                            //ia = 1
+				break;
+
+			case 7:                                            //ia = 2
+				break;
+
+			case 8:                                            //ia = 1
+				break;
+
+			case 9:                                            //ia = 2
+				break;
+
 		}
 		return d;
 	}
